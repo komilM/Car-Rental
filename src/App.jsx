a7400d83bd6dab473cdff57b4c924f3f88dc42fa
komilM <komiljonmeliyev315@gmail.com>
2026-02-24 20:29:54 +0500

    navbar tugadi

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home/Home'
+import Vehicles from './Vehicles/Vehicles'
+import Details from './Details/Details'
+import About from './About/About'
+import Contact from './Contact/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/vehicles' element={<Vehicles />} />
+        <Route path='/details' element={<Details />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Home.css"
+import { NavLink } from 'react-router-dom'
+import logo from "../assets/carlogo.png"
+import contactimg from "../assets/Icon+bg.png"
+
+const Home = () => {
+    return (
+        <div>
+            <nav>
+                <div className="nav-logo">
+                    <img src={logo} alt="" />
+                    <p>Car Rental </p>
+                </div>
+                <div className="nav-texts">
+                    <NavLink to="/" className="navtext">Home</NavLink>
+                    <NavLink to="/vehicles" className="navtext">Vehicles</NavLink>
+                    <NavLink to="/details" className="navtext">Details</NavLink>
+                    <NavLink to="/about" className="navtext">About Us</NavLink>
+                    <NavLink to="/contact" className="navtext">Contact Us</NavLink>
+                </div>
+                <div className="nav-contact">
+                    <img src={contactimg} alt="" />
+                    <div className="nav-contact-texts">
+                        <p>Need help?</p>
+                        <h2>+996 247-1680</h2>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Home
