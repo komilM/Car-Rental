0f42e309fca8302f5fd475e27835f5966b1a499d
komilM <komiljonmeliyev315@gmail.com>
2026-02-25 08:40:38 +0500

    dropdown qilindi

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,15 +4,21 @@ import { NavLink } from 'react-router-dom'
 import logo from "../assets/carlogo.png"
 import contactimg from "../assets/Icon+bg.png"
 import headerbgimg from "../assets/Group 2.png"
+import swithc from "../assets/selectarrow.png"
 
 const Home = () => {
     return (
-        <div>
+        <div className='body'>
+
+            {/* Nav */}
+
             <nav>
+
                 <div className="nav-logo">
                     <img src={logo} alt="" />
                     <p>Car Rental </p>
                 </div>
+
                 <div className="nav-texts">
                     <NavLink to="/" className="navtext">Home</NavLink>
                     <NavLink to="/vehicles" className="navtext">Vehicles</NavLink>
@@ -20,6 +26,7 @@ const Home = () => {
                     <NavLink to="/about" className="navtext">About Us</NavLink>
                     <NavLink to="/contact" className="navtext">Contact Us</NavLink>
                 </div>
+
                 <div className="nav-contact">
                     <img src={contactimg} alt="" />
                     <div className="nav-contact-texts">
@@ -27,7 +34,13 @@ const Home = () => {
                         <h2>+996 247-1680</h2>
                     </div>
                 </div>
+
             </nav>
+
+            {/* Nav */}
+
+            {/* Header */}
+
             <header>
                 <div className="header-box" style={{ backgroundImage: `url(${headerbgimg})` }}>
                     <div className="header-box-texts">
@@ -37,9 +50,70 @@ const Home = () => {
                     </div>
                     <div className="header-box-register">
                         <h1>Book your car</h1>
+
+                        {/* Car dropdown */}
+
+                        <div className="dropdown">
+                            <div className="dropdown-texts">
+                                <h6>Car</h6>
+
+                                <img style={{ width: "16px", height: "16px", marginTop: "12px" }} src={swithc} alt="" />
+                            </div>
+
+                            <ul className="dropdown-list">
+                                <p>Sedan</p>
+                                <p>SUV</p>
+                                <p>Sport</p>
+                                <p>Electric</p>
+                            </ul>
+                        </div>
+
+                        {/* Car dropdown */}
+
+                        {/* Rental place dropdown */}
+
+                        <div className="dropdown">
+                            <div className="dropdown-texts-3    ">
+                                <h6>Place of rental</h6>
+
+                                <img style={{ width: "16px", height: "16px", marginTop: "12px" }} src={swithc} alt="" />
+                            </div>
+
+                            <ul className="dropdown-list">
+                                <p>Sedan</p>
+                                <p>SUV</p>
+                                <p>Sport</p>
+                                <p>Electric</p>
+                            </ul>
+                        </div>
+
+                        {/* Rental place dropdown */}
+
+                        {/* Return place dropdown */}
+
+                        <div className="dropdown">
+                            <div className="dropdown-texts-3">
+                                <h6>Place of return</h6>
+
+                                <img style={{ width: "16px", height: "16px", marginTop: "12px" }} src={swithc} alt="" />
+                            </div>
+
+                            <ul className="dropdown-list">
+                                <p>Sedan</p>
+                                <p>SUV</p>
+                                <p>Sport</p>
+                                <p>Electric</p>
+                            </ul>
+                        </div>
+
+                        {/* Return place dropdown */}
+
                     </div>
                 </div>
             </header>
+
+            {/* Header */}
+
         </div>
     )
 }
