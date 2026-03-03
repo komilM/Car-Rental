c71b47795d1b64912896f5901c17642ea37d8260
komilM <komiljonmeliyev315@gmail.com>
2026-03-03 10:48:32 +0500

    info page tayyor

--- src/Info/Info.jsx
+++ src/Info/Info.jsx
@@ -20,6 +20,11 @@ import carfoot from "../assets/Group 33.png"
 import main3img from "../assets/Group 12.png"
 import footerimg from "../assets/Footer.png"
 import sectionimgc from "../assets/Img788.png"
+import infoitemimg1 from "../assets/gear-shift 1.png"
+import infoitemimg2 from "../assets/Clip path group 11.png"
+import infoitemimg3 from "../assets/g3353.png"
+import infoitemimg4 from "../assets/Mask group45555.png"
+import infoitemimg5 from "../assets/free-icon-distance-9709725 1.png"
 
 
 const Info = () => {
@@ -77,13 +82,51 @@ const Info = () => {
             {/* Nav */}
 
             <div className="sectioninfo">
-                <h1 style={{ fontSize: "40px", fontWeight: "700" }}>{mahsulot.name}</h1>
-                <p style={{ fontSize: "40px", fontWeight: "600", color: "#5937E0" }}>{mahsulot.price}</p>
-                <img src={car} alt="" />
-                <div className="section-info-imgs">
-                <img style={{width: "140px", height:"100px"}} src={sectionimgc} alt="" />
-                <img style={{width: "140px", height:"100px", marginLeft: "24px"}} src={sectionimgc} alt="" />
-                <img style={{width: "140px", height:"100px", marginLeft: "24px"}} src={sectionimgc} alt="" />
+                <div className="section-info-main">
+                    <h1 style={{ fontSize: "40px", fontWeight: "700" }}>{mahsulot.name}</h1>
+                    <p style={{ fontSize: "40px", fontWeight: "600", color: "#5937E0", marginTop: "24px" }}>{mahsulot.price}</p>
+                    <img src={car} alt="" />
+                    <div className="section-info-imgs" style={{marginTop: "50px"}}>
+                        <img style={{ width: "140px", height: "100px", transition: "0.3s" }} src={sectionimgc} alt="" />
+                        <img style={{ width: "140px", height: "100px", marginLeft: "24px", transition: "0.3s" }} src={sectionimgc} alt="" />
+                        <img style={{ width: "140px", height: "100px", marginLeft: "24px", transition: "0.3s" }} src={sectionimgc} alt="" />
+                    </div>
+                </div>
+                <div className="section-info-texts">
+                    <h1>Technical Specification</h1>
+                    <div className="section-info-texts-items">
+                        <div className="section-info-item">
+                            <img src={infoitemimg1} alt="" />
+                            <h2>Gear Box</h2>
+                            <p>Automat</p>
+                        </div>
+                        <div className="section-info-item">
+                            <img src={infoitemimg2} alt="" />
+                            <h2>Fuel</h2>
+                            <p>Petrol</p>
+                        </div>
+                        <div className="section-info-item">
+                            <img src={infoitemimg3} alt="" />
+                            <h2>Doors</h2>
+                            <p>2</p>
+                        </div>
+                        <div className="section-info-item">
+                            <img src={infoitemimg1} alt="" />
+                            <h2>Air Conditioner</h2>
+                            <p>Yes</p>
+                        </div><div className="section-info-item">
+                            <img src={infoitemimg4} alt="" />
+                            <h2>Seats</h2>
+                            <p>5</p>
+                        </div>
+                        <div className="section-info-item">
+                            <img src={infoitemimg5} alt="" />
+                            <h2>Distance</h2>
+                            <p>500</p>
+                        </div>
+
+                    </div>
+                    <button>Rent a car</button>
                 </div>
             </div>
 
